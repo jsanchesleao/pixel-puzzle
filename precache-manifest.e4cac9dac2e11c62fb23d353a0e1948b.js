6e1dd1bf6d7a03beea33a58c80faa55eab4414da
Jeferson <jeferson.silva@rakuten.com>
2020-10-14 15:01:53 +0200

    Updates

---
+++ precache-manifest.e4cac9dac2e11c62fb23d353a0e1948b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abfa6c2d5cf97c4377227544bb3c4da",
+    "url": "/pixel-puzzle/index.html"
+  },
+  {
+    "revision": "9b311d2dec395083e2a7",
+    "url": "/pixel-puzzle/static/css/main.b88b8896.chunk.css"
+  },
+  {
+    "revision": "ed249eef3865870dc5d8",
+    "url": "/pixel-puzzle/static/js/2.897d06f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pixel-puzzle/static/js/2.897d06f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b311d2dec395083e2a7",
+    "url": "/pixel-puzzle/static/js/main.2bf99c1a.chunk.js"
+  },
+  {
+    "revision": "f263c07ca6be4e9e5a32",
+    "url": "/pixel-puzzle/static/js/runtime-main.e1259434.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4902ce428bf4567b2edfa791ac646429.js"
+  "/pixel-puzzle/precache-manifest.e4cac9dac2e11c62fb23d353a0e1948b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pixel-puzzle/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
